1b53ebf475051882d16a16b761f7a0501db4e8cd
Matt Windle <matthew.windle26@gmail.com>
2023-12-14 22:01:39 +0000

    Add: Page content

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,20 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <main className="">
-      <h1>Hello World</h1>
-      <UserButton afterSignOutUrl="/" />
+      <div className="dark:bg-slate-800 bg-[#3984ff] py-16 p-6 dark:text-white text-gray-100 flex ">
+        <div>
+          <h1 className="text-6xl font-bold mb-6">Welcome to Dropbox</h1>
+          <p className="text-4xl font-semibold mb-4">
+            Store everything you need all in one place...
+          </p>
+          <p>
+            Bring your entire workflow together on one integrated platform that
+            works with the tools you already use. Edit PDFs, share videos, sign
+            documents and collaborate seamlessly with internal and external
+            stakeholders – all without leaving Dropbox.
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fjord.dropboxstatic.com",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
