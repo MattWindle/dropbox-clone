ef297a6a05b0e35ee58d3d9b984de06077294b41
Matt Windle <matthew.windle26@gmail.com>
2023-12-14 21:39:24 +0000

    Add: initialized darkmode use shadcn
    Added a darkmode theme toggle using chadcn, this is using the ThemeProvider and the toggle component ThemeToggle.
    
    The provider has been used to wrap the components in the layout and the toggle has been added to the header component

--- components/header.tsx
+++ components/header.tsx
@@ -2,17 +2,19 @@ import Link from "next/link";
 import Image from "next/image";
 import React from "react";
 import { SignIn, SignInButton, SignedOut, UserButton } from "@clerk/nextjs";
+import { ThemeToggle } from "./ThemeToggle";
 
 function Header() {
   return (
     <header className="flex items-center justify-between">
-      <Link href="/" className="flex items-center gap-4">
+      <Link href="/" className="flex items-center gap-2">
         <div>
           <Image
             src="https://upload.wikimedia.org/wikipedia/commons/7/78/Dropbox_Icon.svg"
             alt="logo"
             height={50}
             width={50}
+            className="p-2"
           />
         </div>
         <h1 className="font-bold text-xl">Dropbox</h1>
@@ -20,6 +22,7 @@ function Header() {
       <div className="px-5 flex space-x-2 items-center">
         {/* Theme Toggle */}
         <UserButton afterSignOutUrl="/" />
+        <ThemeToggle />
         <SignedOut>
           <SignInButton afterSignInUrl="/dashboard" mode="modal" />
         </SignedOut>
