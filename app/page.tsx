9c35a1169de2380383e4ac5bd2c6368a55092f6d
Matt Windle <matthew.windle26@gmail.com>
2023-12-20 15:53:18 +0000

    Add: Dummie content area

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import { ArrowRight, Cloud, File, Lock, Server } from "lucide-react";
 
 export default function Home() {
   return (
@@ -44,12 +46,72 @@ export default function Home() {
           Bring your entire workflow together on one integrated platform that works with the tools you already use. Edit PDFs, share videos, sign documents and collaborate seamlessly with internal and external stakeholders – all without leaving Dropbox.
           </p>
           <div className="mt-8">
-            <a
-              href="#"
-              className="inline-flex rounded-md bg-blue-800 px-3.5 py-2.5 font-semibold text-white shadow-sm hover:bg-white/20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+            <Link
+              href="/dashboard"
+              className="inline-flex gap-2 items-center rounded-md bg-blue-800 px-3.5 py-2.5 font-semibold text-white shadow-sm hover:bg-blue-800/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
             >
               Get started now
-            </a>
+              <ArrowRight className="h-4" />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="relative bg-white">
+      <div className="mx-auto max-w-7xl lg:flex lg:justify-between lg:px-8 xl:justify-end">
+        <div className="lg:flex lg:w-1/2 lg:shrink lg:grow-0 xl:absolute xl:inset-y-0 xl:right-1/2 xl:w-1/2">
+          <div className="relative h-80 lg:-ml-8 lg:h-auto lg:w-full lg:grow xl:ml-0">
+            <img
+              className="absolute inset-0 h-full w-full bg-gray-50 object-cover"
+              src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-x=.4&w=2560&h=3413&&q=80"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="px-6 lg:contents">
+          <div className="mx-auto max-w-2xl pb-24 pt-16 sm:pb-32 sm:pt-20 lg:ml-8 lg:mr-0 lg:w-full lg:max-w-lg lg:flex-none lg:pt-32 xl:w-1/2">
+            <p className="text-base font-semibold leading-7 text-indigo-600">Work Better</p>
+            <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Go from idea to done with Dropbox</h1>
+            <p className="mt-6 text-xl leading-8 text-gray-700">
+            Save time, collaborate seamlessly, and deliver work faster...
+            </p>
+            <div className="mt-10 max-w-xl text-base leading-7 text-gray-700 lg:max-w-none">
+              <p>
+              on a platform that works with your existing tools. Dropbox helps you finalize video, image, and audio projects without the hassle. Plus, edit PDFs, share trackable document links, and get agreements signed quickly—all without leaving Dropbox.
+              </p>
+              <ul role="list" className="mt-8 space-y-8 text-gray-600">
+                <li className="flex gap-x-3">
+                  <File className="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">Simple organisation</strong> Keep all your files in one place and organise it just the way you want.
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <Server className="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">Anytime access</strong> Get to your files and photos wherever and whenever you need them – on desktop, mobile or web.
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <Lock className="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">Seamless security</strong> Be sure your content is protected across all your devices with industry-leading encryption, automatic backup and two-factor authentication.
+                  </span>
+                </li>
+              </ul>
+              <p className="mt-8">
+                Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor
+                fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac
+                adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.
+              </p>
+              <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">No server? No problem.</h2>
+              <p className="mt-6">
+                Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh.
+                Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed
+                tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam
+                turpis ipsum eu a sed convallis diam.
+              </p>
+            </div>
           </div>
         </div>
       </div>
